a52e38de59ea4e085ff78a7f7a8e38aaeb74cc35
Frédéric SICRE <frederic.sicre@educlever.com>
2016-05-19 12:26:25 +0200

    add EC_ENV dependant configs

--- app/config/local/app.php
+++ app/config/local/app.php
@@ -1,9 +1,26 @@
 <?php
 
-return array(
+$env = getenv('EC_ENV');
+if (empty($env)) {
+    $env = 'dev';
+}
 
-    'debug' => false,
-    'url' => 'http://localhost/',
-    'key' => 'z5k23G2ppin4NGC8xbyzox6UujZ0GbC5',
+switch ($env) {
 
-);
+    case 'dev':
+        return array(
+            'debug' => false,
+            'url' => 'http://lrs.edu/',
+            'key' => 'a4k23G2pNpi4ngc8xbyZox6UujZ0GyEz',
+        );
+
+    case 'demo':
+        return array(
+            'debug' => false,
+            'url' => 'http://demo.lrs.educlever.io/',
+            'key' => 'z5k23G2ppin4NGC8xbyzox6UujZ0GbC5',
+        );
+
+    default :
+        throw new \Exception("No config for env \"{$env}\"");
+}
--- app/config/local/database.php
+++ app/config/local/database.php
@@ -1,14 +1,40 @@
 <?php
 
-return [
-    'connections' => [
-        'mongodb' => [
-            'driver' => 'mongodb',
-            'host' => 'localhost',
-            'port' => 27017,
-            'username' => '',
-            'password' => '',
-            'database' => 'test',
-        ],
-    ],
-];
+$env = getenv('EC_ENV');
+if (empty($env)) {
+    $env = 'dev';
+}
+
+switch ($env) {
+
+    case 'dev':
+        return [
+            'connections' => [
+                'mongodb' => [
+                    'driver' => 'mongodb',
+                    'host' => 'localhost',
+                    'port' => 27017,
+                    'username' => '',
+                    'password' => '',
+                    'database' => 'lrs',
+                ],
+            ],
+        ];
+
+    case 'demo':
+        return [
+            'connections' => [
+                'mongodb' => [
+                    'driver' => 'mongodb',
+                    'host' => 'localhost',
+                    'port' => 27017,
+                    'username' => '',
+                    'password' => '',
+                    'database' => 'lrs_demo',
+                ],
+            ],
+        ];
+
+    default :
+        throw new \Exception("No config for env \"{$env}\"");
+}
