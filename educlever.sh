9d3b3ab3526753fd673d465d3cf3741ad12cd591
Frédéric SICRE <frederic.sicre@educlever.com>
2016-05-27 11:15:17 +0200

    ajoute la prise en compte du nom de l'action courante (cf educlever/ci release.php)

--- educlever.sh
+++ educlever.sh
@@ -1,29 +1,35 @@
 #!/bin/sh
 
 dir=$(dirname $0)
+action=${1}
 
-exists=$(grep php-amqplib ${dir}/composer.json)
-if [ -z "$exists" ]
-then
-    composer require php-amqplib/php-amqplib
-fi
+case ${action} in
 
-composer install
+    install)
+        exists=$(grep php-amqplib ${dir}/composer.json)
+        if [ -z "$exists" ]
+        then
+            composer require php-amqplib/php-amqplib
+        fi
 
-exists=$(grep educlever-listeners.php ${dir}/app/listeners.php)
-if [ -z "$exists" ]
-then
-    cat << EOF >> ${dir}/app/listeners.php
+        composer install
+
+        exists=$(grep educlever-listeners.php ${dir}/app/listeners.php)
+        if [ -z "$exists" ]
+        then
+            cat << EOF >> ${dir}/app/listeners.php
 require_once(__DIR__ . '/../educlever-listeners.php');
 EOF
-fi
+        fi
 
-chmod a+rw -R ${dir}/app/storage/
+        chmod a+rw -R ${dir}/app/storage/
 
-view=app/views/layouts/loggedout.blade.php
-exists=$(grep "Logo Educlever" ${view})
-if [ -z "$exists" ]
-then
-    sed -i -E 's#(<div class="logo">)#\1<img src="http://demo.educlever.io/img.png" alt="Logo Educlever">#' ${view}
-fi
+        view=app/views/layouts/loggedout.blade.php
+        exists=$(grep "Logo Educlever" ${view})
+        if [ -z "$exists" ]
+        then
+            sed -i -E 's#(<div class="logo">)#\1<img src="http://demo.educlever.io/img.png" alt="Logo Educlever">#' ${view}
+        fi
 
+;;
+esac
