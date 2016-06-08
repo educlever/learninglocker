6f2d4206d489e0eba813d3a82c0d38a68593ed69
Frédéric SICRE <frederic.sicre@educlever.com>
2016-06-08 15:14:47 +0200

    active le debug pour EC_ENV dev et demo

--- app/config/local/app.php
+++ app/config/local/app.php
@@ -9,14 +9,14 @@ switch ($env) {
 
     case 'dev':
         return array(
-            'debug' => false,
+            'debug' => true,
             'url' => 'http://lrs.edu/',
             'key' => 'a4k23G2pNpi4ngc8xbyZox6UujZ0GyEz',
         );
 
     case 'demo':
         return array(
-            'debug' => false,
+            'debug' => true,
             'url' => 'http://demo.lrs.educlever.io/',
             'key' => 'z5k23G2ppin4NGC8xbyzox6UujZ0GbC5',
         );
